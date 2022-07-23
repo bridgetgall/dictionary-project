d472d3e855a20186bccf0d5786b1f910fc3a51d3
bridgetgall <bridgetgalla@gmail.com>
2022-07-23 15:15:11 -0500

    Added search function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import sunset from "./sunset.jpg";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={sunset} className="sunset-image img-fluid" alt="sunset" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={sunset} className="sunset-image img-fluid" alt="sunset" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Bridget Gallagher</footer>
+      </div>
     </div>
   );
 }
